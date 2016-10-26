51289804cb170f84a5a1efe12cbdce7c14a8d29f
tuguoyi <tuguoyi@idealsee.cn>
2016-10-26 16:32:29 +0800

    CTMuxer

--- common/video/CTMuxer.cpp
+++ common/video/CTMuxer.cpp
@@ -3,84 +3,195 @@
 
 CTMuxer::CTMuxer()
 {
-	ofmt_ctx = NULL;
-	ofmt = NULL;
-	video_stream = NULL;
-	audio_stream = NULL;
-	memset(m_output_filename, 0, CTMUXER_MAX_PATH);
+	memset(m_output_streams, 0, sizeof(m_output_streams) * CTMUXER_NUM_STREAM_TYPE);
+	memset(m_input_stream_infos, 0, sizeof(m_input_stream_infos));
+	memset(m_input_stream_time_bases, 0, sizeof(m_input_stream_time_bases));
+	
+	m_aacbsfc = NULL;
+	m_h264bsfc = NULL;
 	m_inited = 0;
-
 }
 
 
 CTMuxer::~CTMuxer()
 {
+	vector<CTMuxerOutput>::iterator iter;
+	for (iter = m_outputs.begin(); iter != m_outputs.end(); iter++) {
+		//Write file trailer
+		if (m_inited) {
+			av_write_trailer(iter->ofmt_ctx);
+		}
+		if(iter->ofmt_ctx && !(iter->ofmt_ctx->oformat->flags & AVFMT_NOFILE))
+			avio_close(iter->ofmt_ctx->pb);
+		avformat_free_context(iter->ofmt_ctx);
+	}
+	if (m_h264bsfc) {
+		av_bitstream_filter_close(m_h264bsfc);
+	}
+	if (m_aacbsfc) {
+		av_bitstream_filter_close(m_aacbsfc);
+	}
 }
 
-int CTMuxer::SetOutputFileName(char *output_filename)
+int CTMuxer::AddOutputFileName(const char *output_filename)
 {
-	strncpy(m_output_filename, output_filename, sizeof(m_output_filename));
-	if (ofmt_ctx) {
-		avformat_free_context(ofmt_ctx);
+	if (!output_filename|| output_filename[0] == 0) {
+		return -1;
 	}
 
-	avformat_alloc_output_context2(&ofmt_ctx, NULL, NULL, m_output_filename);
-	if (!ofmt_ctx) {
+	vector<CTMuxerOutput>::iterator iter;
+	for (iter = m_outputs.begin(); iter != m_outputs.end(); iter++) {
+		if (strcmp(iter->filename, output_filename) == 0) {
+			printf("url[%s] has already existed in CTMuxer\n", output_filename);
+			return 0;
+		}
+	}
+
+	CTMuxerOutput output;
+	memset(&output, 0, sizeof(output));
+	strncpy(output.filename, output_filename, sizeof(output.filename));
+	avformat_alloc_output_context2(&output.ofmt_ctx, NULL, NULL, output.filename);
+	if (!output.ofmt_ctx) {
 		printf("Could not create output context\n");
 		return -1;
 	}
+	strncpy(output.container_format, output.ofmt_ctx->oformat->name, sizeof(output.container_format));
+
+	if (strstr(output.ofmt_ctx->oformat->name, "flv") != NULL ||
+		strstr(output.ofmt_ctx->oformat->name, "mp4") != NULL ||
+		strstr(output.ofmt_ctx->oformat->name, "mkv") != NULL) {
+		output.need_audio_bitstream_filter = 1;
+		output.need_video_bitstream_filter = 1;
+	}
+
+	m_outputs.push_back(output);
 
 	return 0;
 }
 
-int CTMuxer::SetVideoCodecContext(AVCodecContext *codec)
+int CTMuxer::AddInputStreamInfo(int stream_type, AVCodecContext *codec_ctx, AVRational time_base)
 {
-	video_stream = CreateNewStream(codec);
-	if (!video_stream) {
-		printf("Failed create video stream\n");
+	if (stream_type < 0 || stream_type >= CTMUXER_NUM_STREAM_TYPE) {
 		return -1;
 	}
-	return 0;
-}
 
-int CTMuxer::SetAudioCodecContext(AVCodecContext *codec)
-{
-	audio_stream = CreateNewStream(codec);
-	if (!audio_stream) {
-		printf("Failed create video stream\n");
+	vector<CTMuxerOutput>::iterator iter;
+	for (iter = m_outputs.begin(); iter != m_outputs.end(); iter++) {
+		AVStream *stream = CreateStream(iter->ofmt_ctx, codec_ctx);
+		if (!stream) {
+			printf("Failed create video stream\n");
+			return -1;
+		}
+		iter->streams[stream_type] = stream;
+	}
+
+	m_input_stream_time_bases[stream_type] = time_base;
+
+	if (0 != InitBitstreamFilter(stream_type, codec_ctx->codec_id)) {
+		printf("failed to init bitstream_filter for stream_type[%d]\n", stream_type);
 		return -1;
 	}
+
 	return 0;
 }
 
-int CTMuxer::MuxVideo(AVPacket *video_pkt)
+int CTMuxer::AddInputStreamInfo(int stream_type, CTMuxerStreamInfo *stream_info)
 {
+	if (stream_type < 0 || stream_type >= CTMUXER_NUM_STREAM_TYPE) {
+		return -1;
+	}
+	
+	vector<CTMuxerOutput>::iterator iter;
+	for (iter = m_outputs.begin(); iter != m_outputs.end(); iter++) {
+		AVStream *stream = CreateStream(iter->ofmt_ctx, stream_info);
+		if (!stream) {
+			printf("Failed create video stream\n");
+			return -1;
+		}
+		iter->streams[stream_type] = stream;
+	}
+
+	if (0 != InitBitstreamFilter(stream_type, stream_info->codec_id)) {
+		printf("failed to init bitstream_filter for stream_type[%d]\n", stream_type);
+		return -1;
+	}
+
+	m_input_stream_infos[stream_type] = *stream_info;
+	m_input_stream_time_bases[stream_type] = stream_info->time_base;
+	return 0;
+}
+
+int CTMuxer::Mux(int stream_type, AVPacket *input_pkt)
+{
+	int ret;
+
+	if (stream_type < 0 || stream_type >= CTMUXER_NUM_STREAM_TYPE) {
+		return -1;
+	}
+
 	if (!m_inited) {
 		Init();
 	}
+	
+	printf("Write 1 Packet. size:%5d\tpts:%lld\n", input_pkt->size, input_pkt->pts);
 
+	AVRational in_time_base = m_input_stream_time_bases[stream_type];
+	vector<CTMuxerOutput>::iterator iter;
+	for (iter = m_outputs.begin(); iter != m_outputs.end(); iter++) {
+		AVPacket packet;
+		AVPacket *pkt = &packet;
+		av_copy_packet(pkt, input_pkt);
 
+		AVStream *out_stream = iter->streams[stream_type];
+		if (stream_type == CTMUXER_STREAM_TYPE_AUDIO && m_aacbsfc &&
+			iter->need_audio_bitstream_filter) {
+			ret = av_bitstream_filter_filter(m_aacbsfc, out_stream->codec, NULL, &pkt->data, &pkt->size, pkt->data, pkt->size, 0);
+		} else if (stream_type == CTMUXER_STREAM_TYPE_VIDEO && m_h264bsfc &&
+			iter->need_video_bitstream_filter) {
+			av_bitstream_filter_filter(m_h264bsfc, out_stream->codec, NULL, &pkt->data, &pkt->size, pkt->data, pkt->size, 0);
+		}
 
-	return 0;
-}
+		pkt->stream_index = out_stream->index;
+		pkt->pos = -1;
+		pkt->dts  = av_rescale_q_rnd(pkt->dts, in_time_base, out_stream->time_base, (AVRounding)(AV_ROUND_NEAR_INF | AV_ROUND_PASS_MINMAX));
+		pkt->pts  = av_rescale_q_rnd(pkt->pts, in_time_base, out_stream->time_base, (AVRounding)(AV_ROUND_NEAR_INF | AV_ROUND_PASS_MINMAX));
+		pkt->duration = av_rescale_q(pkt->duration, in_time_base, out_stream->time_base);
 
-int CTMuxer::MuxAudio(AVPacket *audio_pkt)
-{
-	if (!m_inited) {
-		Init();
+		//Write
+		int ret = av_interleaved_write_frame(iter->ofmt_ctx, pkt);
+		av_packet_unref(pkt);
+		if (ret < 0) {
+			printf("Error muxing packet\n");
+			return -1;
+		}
 	}
-
 	return 0;
 }
 
 int CTMuxer::Init()
 {
-	avformat_write_header(ofmt_ctx, NULL);
+	vector<CTMuxerOutput>::iterator iter;
+	for (iter = m_outputs.begin(); iter != m_outputs.end(); iter++) {
+		printf("==========Output Information==========\n");
+		av_dump_format(iter->ofmt_ctx, 0, iter->filename, 1);
+		printf("======================================\n");
+
+		//Open output file
+		if (!(iter->ofmt_ctx->oformat->flags & AVFMT_NOFILE)) {
+			if (avio_open(&iter->ofmt_ctx->pb, iter->filename, AVIO_FLAG_WRITE) < 0) {
+				printf("Could not open output file '%s'", iter->filename);
+				return -1;
+			}
+		}
+
+		avformat_write_header(iter->ofmt_ctx, NULL);
+	}
+
 	m_inited = 1;
 	return 0;
 }
 
-AVStream *CTMuxer::CreateNewStream(AVCodecContext *codec)
+AVStream *CTMuxer::CreateStream(AVFormatContext *ofmt_ctx, AVCodecContext *codec_ctx)
 {
 	AVStream *new_stream = avformat_new_stream(ofmt_ctx, NULL);
 	if (!new_stream) {
@@ -88,7 +199,7 @@ AVStream *CTMuxer::CreateNewStream(AVCodecContext *codec)
 		return NULL;
 	}
 
-	if (avcodec_copy_context(new_stream->codec, codec) < 0) {
+	if (avcodec_copy_context(new_stream->codec, codec_ctx) < 0) {
 		printf("Failed to copy context from input to output stream codec context\n");
 		return NULL;
 	}
@@ -98,4 +209,58 @@ AVStream *CTMuxer::CreateNewStream(AVCodecContext *codec)
 		new_stream->codec->flags |= CODEC_FLAG_GLOBAL_HEADER;
 
 	return new_stream;
+}
+
+
+AVStream *CTMuxer::CreateStream(AVFormatContext *ofmt_ctx, CTMuxerStreamInfo *stream_info)
+{
+	AVStream *new_stream = avformat_new_stream(ofmt_ctx, NULL);
+	if (!new_stream) {
+		printf("Failed allocating output stream\n");
+		return NULL;
+	}
+
+	new_stream->codec->codec_id = stream_info->codec_id;
+	new_stream->codec->codec_type = stream_info->codec_type;
+	if (stream_info->codec_type == AVMEDIA_TYPE_VIDEO) {
+		new_stream->codec->pix_fmt = stream_info->pix_fmt;
+		new_stream->codec->width = stream_info->width; // dimension must be set
+		new_stream->codec->height = stream_info->height;
+		new_stream->codec->time_base.num = 1;
+		new_stream->codec->time_base.num = stream_info->fps;  // time_base must be set
+	}
+	else {
+		new_stream->codec->sample_fmt = stream_info->sample_fmt;
+		new_stream->codec->sample_rate = stream_info->sample_rate;
+		new_stream->codec->channel_layout = stream_info->channel_layout;
+		new_stream->codec->channels = stream_info->channels;
+		new_stream->codec->bit_rate = stream_info->bit_rate;
+	}
+
+	new_stream->codec->codec_tag = 0;
+	if (ofmt_ctx->oformat->flags & AVFMT_GLOBALHEADER)
+		new_stream->codec->flags |= CODEC_FLAG_GLOBAL_HEADER;
+
+	return new_stream;
+}
+
+int CTMuxer::InitBitstreamFilter(int stream_type, AVCodecID codec_id)
+{
+	if (stream_type == CTMUXER_STREAM_TYPE_AUDIO && codec_id == AV_CODEC_ID_AAC) {
+		if (!m_aacbsfc) {
+			m_aacbsfc = av_bitstream_filter_init("aac_adtstoasc");
+			if (!m_aacbsfc) {
+				return -1;
+			}
+		}
+	} else if (stream_type == CTMUXER_STREAM_TYPE_VIDEO && codec_id == AV_CODEC_ID_H264) {
+		if (!m_h264bsfc) {
+			m_h264bsfc = av_bitstream_filter_init("h264_mp4toannexb");
+			if (!m_h264bsfc) {
+				return -1;
+			}
+		}
+	}
+
+	return 0;
 }
--- common/video/CTMuxer.h
+++ common/video/CTMuxer.h
@@ -19,30 +19,76 @@ extern "C"
 #endif
 #endif
 
-#define CTMUXER_MAX_PATH	256
+#include <vector>
+using namespace std;
+
+#define CTMUXER_MAX_CONTAINER_FORMAT_SIZE	64
+#define CTMUXER_MAX_PATH				256
+// #define CTMUXER_
+
+enum 
+{
+	CTMUXER_STREAM_TYPE_VIDEO = 0,
+	CTMUXER_STREAM_TYPE_AUDIO,
+	CTMUXER_NUM_STREAM_TYPE,
+};
+
+typedef struct CTMuxerStreamInfo {
+	AVCodecID codec_id;
+	AVMediaType codec_type;
+	AVRational time_base;
+
+	// the following four members are used for audio stream
+	AVPixelFormat pix_fmt;
+	int width;
+	int height;
+	int fps;
+
+	// the following five members are used for audio stream
+	AVSampleFormat sample_fmt; // AV_SAMPLE_FMT_S16/AV_SAMPLE_FMT_S16P;;
+	int sample_rate; // 44100
+	uint64_t channel_layout;
+	int channels;
+	int bit_rate; // 64000;
+} CTMuxerStreamInfo;
 
 
+typedef struct CTMuxerOutput {
+	AVFormatContext *ofmt_ctx;
+	AVStream  *streams[CTMUXER_NUM_STREAM_TYPE];
+	char filename[CTMUXER_MAX_PATH];
+	char container_format[CTMUXER_MAX_CONTAINER_FORMAT_SIZE];
+	int  need_audio_bitstream_filter;
+	int  need_video_bitstream_filter;
+};
+
+// note: calling sequence
+//	1. AddOutputFileName()
+//  2. AddInputStreamInfo()
+//  3. MuxPacket()
 class CTMuxer
 {
 public:
 	CTMuxer();
 	virtual ~CTMuxer();
-	int SetOutputFileName(char *output_filename);
-	int SetVideoCodecContext(AVCodecContext *codec);
-	int SetAudioCodecContext(AVCodecContext *codec);
-	int MuxVideo(AVPacket *video_pkt);
-	int MuxAudio(AVPacket *audio_pkt);
+	int AddOutputFileName(const char *output_filename);
+	int AddInputStreamInfo(int stream_type, AVCodecContext *codec_ctx, AVRational time_base);
+	int AddInputStreamInfo(int stream_type, CTMuxerStreamInfo *stream_info);
+	int Mux(int stream_type, AVPacket *pkt);
+
 private:
 	int Init();
-	AVStream *CreateNewStream(AVCodecContext *codec);
+	AVStream *CreateStream(AVFormatContext *ofmt_ctx, AVCodecContext *codec_ctx);
+	AVStream *CreateStream(AVFormatContext *ofmt_ctx, CTMuxerStreamInfo *stream_info);
+	int InitBitstreamFilter(int stream_type, AVCodecID codec_id);
 	
 private:
-	AVFormatContext *ofmt_ctx;
-	AVOutputFormat *ofmt;
-	AVStream *video_stream;
-	AVStream *audio_stream;
-
-	char m_output_filename[CTMUXER_MAX_PATH];
+	AVStream  *m_output_streams[CTMUXER_NUM_STREAM_TYPE];
+	CTMuxerStreamInfo m_input_stream_infos[CTMUXER_NUM_STREAM_TYPE];
+	AVRational	m_input_stream_time_bases[CTMUXER_NUM_STREAM_TYPE];
+	vector<CTMuxerOutput> m_outputs;
+	AVBitStreamFilterContext *m_aacbsfc;
+	AVBitStreamFilterContext* m_h264bsfc;
 	int  m_inited;
 };
 
